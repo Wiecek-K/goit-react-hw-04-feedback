8007331acaf677867c7a697b1f6606e37326b90e
Wiecek-K <krystiandwiecek@gmail.com>
2023-06-11 17:42:08 +0200

    reset repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [goodCnt, setGoodCnt] = useState(0);
+  const [neutralCnt, setNeutralCnt] = useState(0);
+  const [badCnt, setBadCnt] = useState(0);
 
+  const increaseGoodCnt = () => {
+    setGoodCnt((prevState) => prevState + 1);
+  };
+  const increaseNeutralCnt = () => {
+    setNeutralCnt((prevState) => prevState + 1);
+  };
+  const increaseBadCnt = () => {
+    setBadCnt((prevState) => prevState + 1);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Please leave feedback</h2>
+      <button onClick={increaseGoodCnt}>Good</button>
+      <button onClick={increaseNeutralCnt}>Neutral</button>
+      <button onClick={increaseBadCnt}>Bad</button>
+      <h2>Statistic</h2>
+      <p>{`Good: ${goodCnt}`}</p>
+      <p>{`Neutral: ${neutralCnt}`}</p>
+      <p>{`Bad: ${badCnt}`}</p>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
